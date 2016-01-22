db567eef79596cfd68538c32bc4d352b1f61356f
dupanov <vadim.dupanov@gmail.com>
2016-01-22 02:52:41 +0200

    working under maven

--- src/headfirst/combined/djview/BeatObserver.java
+++
@@ -1,8 +0,0 @@
-package headfirst.combined.djview;
-
-/**
- * Created by Вадик on 18.01.2016.
- */
-public interface BeatObserver {
-    void updateBeat();
-}
---
+++ src/test/java/headfirst/combined/djview/DJViewTest.groovy
@@ -0,0 +1,14 @@
+package headfirst.combined.djview
+/**
+ * Created by Вадик on 22.01.2016.
+ */
+class DJViewTest extends GroovyTestCase {
+    void testCreateView() {
+        DJView view = new DJView();
+        assert (view);
+    }
+
+    void testCreateControls() {
+
+    }
+}
